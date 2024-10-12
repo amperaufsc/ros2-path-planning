df5c94b653265e4f00862e15393f376bd83f2e82
Yasmin Avila Nunes <yasmin.nunes@amperaracing.com>
2024-10-12 20:17:35 -0300

    .

--- src/path_planning/ros2/path_subscriber.py
+++ src/path_planning/ros2/path_subscriber.py
@@ -1,39 +1,33 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+from tests.bayesian_inference_tests_csv import Cone
 
-from std_msgs.msg import String
-
-
-class PathPublisher(Node):
+class PathSubscriber(Node):
 
     def __init__(self):
         super().__init__('path_subscriber')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.subscription = self.create_subscription(
+            Image,
+            '/fsds/cameracam1/image_color',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.br = CvBridge()
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-
+    def listener_callback(self, path):
+        self.get_logger().info('I heard: "%d"' % path.width)
+        img = self.br.imgmsg_to_cv2(path)
+        #Processamento de imagem aqui
+        cv2.imshow('Right', img)
+        cv2.waitKey(1)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = PathPublisher()
+    path_subscriber = PathSubscriber()
 
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
+  
